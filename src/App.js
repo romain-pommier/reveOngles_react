a8a31d3c83d089dffb545c34f98722c0d7c5075d
romain-pommier <romain-p31@hottmail.fr>
2020-10-25 19:19:15 +0100

    updtae

--- src/App.js
+++ src/App.js
@@ -1,67 +1,61 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  Link,
-} from 'react-router-dom';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom'
+import './App.css'
 // import PocReact from './Components/PocReact'
-import Header from './Components/Header';
-import IntroDiv from './Components/IntroDiv';
-import DescriptionEntreprise from './Components/DescriptionEntreprise';
-import PerfectFlag from './Components/PerfectFlag';
-import Tarifs from './Components/Tarifs';
-import CarouselMain from './Components/CarouselMain';
-import LoginPage from './Components/LoginPage';
-import Footer from './Components/Footer';
-import './style/style.scss';
-import './style/login.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/accordion.scss';
+import Header from './Components/Header'
+import IntroDiv from './Components/IntroDiv'
+import DescriptionEntreprise from './Components/DescriptionEntreprise'
+import PerfectFlag from './Components/PerfectFlag'
+import Tarifs from './Components/Tarifs'
+import CarouselMain from './Components/CarouselMain'
+import LoginPage from './Components/LoginPage'
+import Footer from './Components/Footer'
+import './style/style.scss'
+import './style/login.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/accordion.scss'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isUserAuthenticated: false,
-    };
-  }
-  render() {
-    return (
-      <>
-        <Router>
-          <Switch>
-            <Route exact path="/admin"></Route>
-            <Route
-              exact
-              path="/login"
-              render={() => {
-                return this.state.isUserAuthenticated ? (
-                  <Redirect to="/" />
-                ) : (
-                  <Redirect to="/admin" />
-                );
-              }}
-            >
-              <LoginPage />
-            </Route>
+    constructor(props) {
+        super(props)
+        this.state = {
+            isUserAuthenticated: false,
+            token: false,
+        }
+    }
+    render() {
+        return (
+            <>
+                <Router>
+                    <Switch>
+                        {/* <Route exact path="/admin"></Route> */}
+                        <Route
+                            exact
+                            path="/admin"
+                            render={() => {
+                                console.log(this.state.isUserAuthenticated)
+                                return this.state.isUserAuthenticated ? (
+                                    <Redirect to="/" />
+                                ) : (
+                                    <LoginPage token={this.token} />
+                                )
+                            }}
+                        ></Route>
 
-            <Route exact path="/">
-              <Header />
-              <IntroDiv />
-              <DescriptionEntreprise />
-              <PerfectFlag />
-              <Tarifs />
-              <CarouselMain />
-              <Footer />
-            </Route>
-          </Switch>
-        </Router>
-      </>
-    );
-  }
+                        <Route exact path="/">
+                            <Header />
+                            <IntroDiv />
+                            <DescriptionEntreprise />
+                            <PerfectFlag />
+                            <Tarifs />
+                            <CarouselMain />
+                            <Footer />
+                        </Route>
+                    </Switch>
+                </Router>
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
