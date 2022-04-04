fa4927f2850abf06c3c26abde45cf5c14ef5d1f5
r.pommier <romain-p31@hotmail.fr>
2022-04-04 10:15:26 +0200

    update text

--- src/Components/DescriptionEntreprise.js
+++ src/Components/DescriptionEntreprise.js
@@ -1,62 +1,68 @@
-import React, { Component } from "react"
-import { Image, Row, Col } from "react-bootstrap"
-import NailPolish from "../image/nail-polish3.png"
-import LogoReveongles from "../image/rsz_logo_reve_ongles.png"
-import LogoBush from "../image/bush_icon3.png"
-import LogoStar from "../image/star_icon3.png"
-import LogoPhone from "../image/phone_icon3.png"
+import React, { Component } from 'react';
+import { Image, Row, Col } from 'react-bootstrap';
+import NailPolish from '../image/nail-polish3.png';
+import LogoReveongles from '../image/rsz_logo_reve_ongles.png';
+import LogoBush from '../image/bush_icon3.png';
+import LogoStar from '../image/star_icon3.png';
+import LogoPhone from '../image/phone_icon3.png';
 
 class DescriptionEntreprise extends Component {
-    render() {
-        return (
-            <Row id="prestations" className="description_div">
-                <div>
-                    <Col className="mt-5 pt-5">
-                        <div className=" d-flex justify-content-center">
-                            <Image
-                                className="nail_polish"
-                                alt="nailpolish logo"
-                                src={NailPolish}
-                                fluid
-                            ></Image>
-                        </div>
-                        <div className=" pt-5 d-flex justify-content-center">
-                            <Image
-                                className="description_logo"
-                                alt="logo rêve d'ongles"
-                                src={LogoReveongles}
-                                fluid
-                            ></Image>
-                        </div>
-                        <h2 className="description_header">
-                            Qui sommes nous ?
-                        </h2>
-                        <p className="pt-5 text-center description_text">
-                            Rêve d’ongles De formation esthéticienne et fascinée
-                            par les Ongles à l’âge de 20 ans je décide de me
-                            lancer à mon compte dans les ongles, en cumulant
-                            deux emplois pour commencer à faire ma clientèle et
-                            tout en ayant un salaire fixe. Je ne compte pas les
-                            heures passées, m’entraînant sans relâche pour
-                            apporter à mes clientes les meilleures des
-                            prestations, j’ai participé à de nombreux stages et
-                            suvit des formations auprès des plus grands artistes
-                            formateurs français et internationaux, travail et
-                            encore le travail…Toujours en quête de perfection.
-                            <br />
-                            <br />
-                            Après douze ans d’expériences, j’ai décidé de créer
-                            mon propre centre de formation et obtenue la charte
-                            qualité Qualiopi de France, en mon nom, avec mes
-                            propres programmes de formation. Je teste et je
-                            sélectionne le meilleur de chacune, pour apporter à
-                            mes clientes et mes élèves, des formations
-                            professionnelles qualifiantes, complètes pour leur
-                            permettre d’atteindre l’excellence « Laissez votre
-                            passion vous mener à votre profession » Au plaisir
-                            de vous rencontrer un jour, lors d’une formation
-                        </p>
-                        {/* <h2 className="description_header">
+	render() {
+		return (
+			<Row id="prestations" className="description_div">
+				<div>
+					<Col className="mt-5 pt-5">
+						<div className=" d-flex justify-content-center">
+							<Image
+								className="nail_polish"
+								alt="nailpolish logo"
+								src={NailPolish}
+								fluid
+							></Image>
+						</div>
+						<div className=" pt-5 d-flex justify-content-center">
+							<Image
+								className="description_logo"
+								alt="logo rêve d'ongles"
+								src={LogoReveongles}
+								fluid
+							></Image>
+						</div>
+						<h2 className="description_header">
+							Qui sommes nous ?
+						</h2>
+						<p className="pt-5  description_text">
+							Rêve d’ongles De formation esthéticienne et fascinée
+							par les Ongles à l’âge de 20 ans je décide de me
+							lancer à mon compte dans les ongles, en cumulant
+							deux emplois pour commencer à faire ma clientèle et
+							tout en ayant un salaire fixe. Je ne compte pas les
+							heures passées, m’entraînant sans relâche pour
+							apporter à mes clientes les meilleures des
+							prestations, j’ai participé à de nombreux stages et
+							suvit des formations auprès des plus grands artistes
+							formateurs français et internationaux, travail et
+							encore le travail…Toujours en quête de perfection.
+							<br />
+							<br />
+							Après douze ans d’expériences, j’ai décidé de créer
+							mon propre centre de formation et obtenue la charte
+							qualité Qualiopi de France, en mon nom, avec mes
+							propres programmes de formation. Je teste et je
+							sélectionne le meilleur de chacune, pour apporter à
+							mes clientes et mes élèves, des formations
+							professionnelles qualifiantes, complètes pour leur
+							permettre d’atteindre l’excellence
+							<br />
+							<br />
+							« Laissez votre passion vous mener à votre
+							profession »
+							<br />
+							<br />
+							Au plaisir de vous rencontrer un jour, lors d’une
+							formation
+						</p>
+						{/* <h2 className="description_header">
                             Salon de manucure et centre de formation
                         </h2>
                         <p className="pt-5 text-center description_text">
@@ -81,55 +87,55 @@ class DescriptionEntreprise extends Component {
                             semi-permanent, nail art, pose artistique) sur
                             rendez-vous.
                         </p> */}
-                    </Col>
-                </div>
-                <div className="w-100 mb-5 py-5">
-                    <Row className="justify-content-center">
-                        <Col className="text-center">
-                            <a href="#realisations">
-                                <div className="text-center">
-                                    <Image
-                                        src={LogoBush}
-                                        fluid
-                                        alt="logo bush"
-                                    ></Image>
-                                </div>
-                                <h6 style={{ color: "black" }}>Préstations</h6>
-                            </a>
-                        </Col>
-                        <Col className="text-center">
-                            <a href="#tarifs">
-                                <div className="text-center">
-                                    <Image
-                                        src={LogoStar}
-                                        fluid
-                                        alt="logo star"
-                                    ></Image>
-                                </div>
-                                <h6
-                                    style={{ color: "black" }}
-                                    className="text-center"
-                                >
-                                    Tarifs
-                                </h6>
-                            </a>
-                        </Col>
-                        <Col className="text-center">
-                            <a href="#footer">
-                                <div className="text-center">
-                                    <Image
-                                        src={LogoPhone}
-                                        fluid
-                                        alt="logo phone"
-                                    ></Image>
-                                </div>
-                                <h6 style={{ color: "black" }}>Contact</h6>
-                            </a>
-                        </Col>
-                    </Row>
-                </div>
-            </Row>
-        )
-    }
+					</Col>
+				</div>
+				<div className="w-100 mb-5 py-5">
+					<Row className="justify-content-center">
+						<Col className="text-center">
+							<a href="#realisations">
+								<div className="text-center">
+									<Image
+										src={LogoBush}
+										fluid
+										alt="logo bush"
+									></Image>
+								</div>
+								<h6 style={{ color: 'black' }}>Préstations</h6>
+							</a>
+						</Col>
+						<Col className="text-center">
+							<a href="#tarifs">
+								<div className="text-center">
+									<Image
+										src={LogoStar}
+										fluid
+										alt="logo star"
+									></Image>
+								</div>
+								<h6
+									style={{ color: 'black' }}
+									className="text-center"
+								>
+									Tarifs
+								</h6>
+							</a>
+						</Col>
+						<Col className="text-center">
+							<a href="#footer">
+								<div className="text-center">
+									<Image
+										src={LogoPhone}
+										fluid
+										alt="logo phone"
+									></Image>
+								</div>
+								<h6 style={{ color: 'black' }}>Contact</h6>
+							</a>
+						</Col>
+					</Row>
+				</div>
+			</Row>
+		);
+	}
 }
-export default DescriptionEntreprise
+export default DescriptionEntreprise;
--- src/Components/Formation.js
+++ src/Components/Formation.js
@@ -1,72 +1,73 @@
-import React, { Component } from "react"
-import Accordion from "./Accordion"
-import FormationIntensive from "../Contents/FormationIntensive"
-import MalLogo from "../image/mal_logo.png"
-import SatisfactionLogo from "../image/satisfaction.png"
-import MentalLogo from "../image/mental_logo.png"
-import FormationGel from "../Contents/FormationGel"
-import FormationFrench from "../Contents/FormationFrench"
-import FormationManucure from "../Contents/FormationManucure"
-import FormationRapide from "../Contents/FormationRapide"
-import FormationBaby from "../Contents/FormationBaby"
-import FormationCoaching from "../Contents/FormationCoaching"
-import FormationMini from "../Contents/FormationMini"
+import React, { Component } from 'react';
+import Accordion from './Accordion';
+import FormationIntensive from '../Contents/FormationIntensive';
+import MalLogo from '../image/mal_logo.png';
+import SatisfactionLogo from '../image/satisfaction.png';
+import MentalLogo from '../image/mental_logo.png';
+import FormationGel from '../Contents/FormationGel';
+import FormationFrench from '../Contents/FormationFrench';
+import FormationManucure from '../Contents/FormationManucure';
+import FormationRapide from '../Contents/FormationRapide';
+import FormationBaby from '../Contents/FormationBaby';
+import FormationCoaching from '../Contents/FormationCoaching';
+import FormationMini from '../Contents/FormationMini';
 class Formation extends Component {
-    data = [
-        {
-            id: 1,
-            name: "Formation intensive débutante, initiation Gel 5 jours",
-            content: () => <FormationIntensive />,
-            state: "inactive",
-        },
-        // {
-        //     id: 2,
-        //     name: "Perfectionnement gel",
-        //     content: () => <FormationGel />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 3,
-        //     name: "French reverse",
-        //     content: () => <FormationFrench />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 4,
-        //     name: "Manucure russe et vernis permanent renfort",
-        //     content: () => <FormationManucure />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 5,
-        //     name: "Pose salon rapide ",
-        //     content: () => <FormationRapide />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 6,
-        //     name: "Babyboomer et dégradé",
-        //     content: () => <FormationBaby />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 7,
-        //     name: "Coaching Privé",
-        //     content: () => <FormationCoaching />,
-        //     state: "inactive",
-        // },
-        // {
-        //     id: 8,
-        //     name: "Mini formes",
-        //     content: () => <FormationMini />,
-        //     state: "inactive",
-        // },
-    ]
-    render() {
-        return (
-            <div id="tarifs" className="accordion_div">
-                <h2 className="m-5 text-center">Formations</h2>
-                {/* <p style={{ paddingRight: "20%", paddingLeft: "20%" }}>
+	data = [
+		{
+			id: 1,
+			name: 'Formation intensive débutante, initiation Gel 5 jours',
+			content: () => <FormationIntensive />,
+			state: 'inactive',
+		},
+		{
+			id: 2,
+			name: 'Perfectionnement gel',
+			content: () => <FormationGel />,
+			state: 'inactive',
+		},
+		{
+			id: 3,
+			name: 'Manucure russe et vernis permanent renfort',
+			content: () => <FormationManucure />,
+			state: 'inactive',
+		},
+		// {
+		//     id: 4,
+		//     name: "French reverse",
+		//     content: () => <FormationFrench />,
+		//     state: "inactive",
+		// },
+
+		// {
+		//     id: 5,
+		//     name: "Pose salon rapide ",
+		//     content: () => <FormationRapide />,
+		//     state: "inactive",
+		// },
+		// {
+		//     id: 6,
+		//     name: "Babyboomer et dégradé",
+		//     content: () => <FormationBaby />,
+		//     state: "inactive",
+		// },
+		// {
+		//     id: 7,
+		//     name: "Coaching Privé",
+		//     content: () => <FormationCoaching />,
+		//     state: "inactive",
+		// },
+		// {
+		//     id: 8,
+		//     name: "Mini formes",
+		//     content: () => <FormationMini />,
+		//     state: "inactive",
+		// },
+	];
+	render() {
+		return (
+			<div id="tarifs" className="accordion_div">
+				<h2 className="m-5 text-center">Formations</h2>
+				{/* <p style={{ paddingRight: "20%", paddingLeft: "20%" }}>
                     Rêve d’ongles est un centre de formation dédié à
                     l'enseignement des techniques de pose d'ongle. Formatrice
                     depuis plus de 4 ans et prise de passion pour les ongles,
@@ -92,39 +93,39 @@ class Formation extends Component {
                     ongulaire ou Perfectionnez-vous ! L'envie de toujours avoir
                     envie d'apprendre et d'évoluer et vous ?
                 </p> */}
-                <Accordion data={this.data}></Accordion>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                        marginTop: "5%",
-                    }}
-                >
-                    <img src={MalLogo} height="100" width="100" />
-                    <img src={MentalLogo} height="100" width="100" />
-                </div>
-                <p className="text-center mt-5">
-                    Demande de renseignements, dates et inscriptions sur ma page{" "}
-                    <a
-                        style={{ color: "#212529" }}
-                        href="https://www.facebook.com/revedongles31/"
-                    >
-                        Facebook: revedonglesanne31
-                    </a>{" "}
-                    ou par{" "}
-                    <a
-                        style={{ color: "#212529" }}
-                        href="mailto:revedonglesformation@gmail.com"
-                    >
-                        {" "}
-                        Mail: revedonglesformation@gmail.com
-                    </a>
-                </p>
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <img src={SatisfactionLogo} alt="Satisfaction Logo" />
-                </div>
-            </div>
-        )
-    }
+				<Accordion data={this.data}></Accordion>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'space-evenly',
+						marginTop: '5%',
+					}}
+				>
+					<img src={MalLogo} height="100" width="100" />
+					<img src={MentalLogo} height="100" width="100" />
+				</div>
+				<p className="text-center mt-5">
+					Demande de renseignements, dates et inscriptions sur ma page{' '}
+					<a
+						style={{ color: '#212529' }}
+						href="https://www.facebook.com/revedongles31/"
+					>
+						Facebook: revedonglesanne31
+					</a>{' '}
+					ou par{' '}
+					<a
+						style={{ color: '#212529' }}
+						href="mailto:revedonglesformation@gmail.com"
+					>
+						{' '}
+						Mail: revedonglesformation@gmail.com
+					</a>
+				</p>
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<img src={SatisfactionLogo} alt="Satisfaction Logo" />
+				</div>
+			</div>
+		);
+	}
 }
-export default Formation
+export default Formation;
